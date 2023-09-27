75616c0d8d93fd87b23c57c51f1cc7b950eeb464
AimbotParce <marcparcerisa@gmail.com>
2023-09-27 23:16:15 +0200

    Hello world

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import "../styles/global.css"
+import React from "react"
+import { motion } from "framer-motion"
+
+const IndexPage = () => {
+    return (
+        <main>
+            <h1>Hello World</h1>
+        </main>
+    )
+}
+
+export default IndexPage
