02ff47a490fd785ea2ffd0bc78c612919aca9390
AimbotParce <marcparcerisa@gmail.com>
2025-02-16 12:58:09 +0100

    A bit of style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import "../styles/global.css"
-import React from "react"
-import Head from "next/head"
 import { motion } from "framer-motion"
-import Person from "../components/Person"
+import Head from "next/head"
+import { useState } from "react"
 import Button from "../components/Button"
+import CloseButton from "../components/CloseButton"
+import Person from "../components/Person"
 import boxes from "../styles/boxes.module.css"
+import "../styles/global.css"
 
 const IndexPage = () => {
-    const [people, setPeople] = React.useState([])
-    const [pair_target, setPairing] = React.useState("")
-    const [avoid_target, setAvoiding] = React.useState("")
-    const [avoidances, setAvoidances] = React.useState([])
-    const [pairings, setPairings] = React.useState([])
+    const [people, setPeople] = useState<string[]>([])
+    const [pair_target, setPairing] = useState<string>("")
+    const [avoid_target, setAvoiding] = useState<string>("")
+    const [avoidances, setAvoidances] = useState<[string, string][]>([])
+    const [pairings, setPairings] = useState<[string, string][]>([])
 
     const onAddPerson = (event) => {
         event.preventDefault()
@@ -36,8 +37,16 @@ const IndexPage = () => {
         setPairings(pairings.filter((pair) => pair[0] !== name && pair[1] !== name))
     }
 
+    const removePairByIndex = (index) => {
+        setPairings(pairings.filter((_, i) => i !== index))
+    }
+
     const removeAvoids = (name) => {
         setAvoidances(avoidances.filter((pair) => pair[0] !== name && pair[1] !== name))
+    }
+
+    const removeAvoidByIndex = (index) => {
+        setAvoidances(avoidances.filter((_, i) => i !== index))
     }
 
     const removePerson = (name) => {
@@ -175,25 +184,45 @@ const IndexPage = () => {
                     </motion.div>
                 </div>
                 <br />
-                <h2 className="text-2xl font-bold">Pairings</h2>
-                <motion.div>
-                    {pairings.map((pair) => (
-                        <motion.div className="flex r gap-10">
-                            <Person name={pair[0]} state="show" />
-                            <Person name={pair[1]} state="show" />
+                {pairings.length > 0 && (
+                    <>
+                        <h2 className="text-2xl font-bold">Pairings</h2>
+                        <motion.div>
+                            {pairings.map((pair, j) => (
+                                <motion.div className="flex r gap-10 items-center">
+                                    <Person name={pair[0]} state="show" />
+                                    <Person name={pair[1]} state="show" />
+
+                                    <CloseButton onClick={() => removePairByIndex(j)} />
+                                </motion.div>
+                            ))}
                         </motion.div>
-                    ))}
-                </motion.div>
-                <br />
-                <h2 className="text-2xl font-bold">Avoidances</h2>
-                <motion.div>
-                    {avoidances.map((pair) => (
-                        <motion.div className="flex r gap-10">
-                            <Person name={pair[0]} state="show" />
-                            <Person name={pair[1]} state="show" />
+                        <br />
+                    </>
+                )}
+
+                {avoidances.length > 0 && (
+                    <>
+                        <h2 className="text-2xl font-bold">Avoidances</h2>
+                        <motion.div>
+                            {avoidances.map((pair, j) => (
+                                <motion.div className="flex r gap-10 items-center" key={j}>
+                                    <Person name={pair[0]} state="show" />
+                                    <Person name={pair[1]} state="show" />
+                                    <CloseButton onClick={() => removeAvoidByIndex(j)} />
+                                </motion.div>
+                            ))}
                         </motion.div>
-                    ))}
-                </motion.div>
+                        <br />
+                    </>
+                )}
+
+                <motion.button
+                    onClick={() => alert("Not implemented!")}
+                    className={`ml-2 text-xl font-bold px-4 py-2 mt-2 ${boxes.cartoony}`}
+                >
+                    <p>Generate Teams</p>
+                </motion.button>
             </main>
         </>
     )
